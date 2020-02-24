49fc43c7b1df0d297168f8bc32ae60f8a68d608b
Vitaliy Bazlytskyi <vbazalickyi@gmail.com>
2020-02-25 01:05:01 +0200

    fixed untracked files

---
+++ src/test/java/com/bazalytskyi/hw2_distribured/Hw2DistriburedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazalytskyi.hw2_distribured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2DistriburedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
